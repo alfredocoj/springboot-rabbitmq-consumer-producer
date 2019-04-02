ebbdc29ad93673b86486b896789d96a5d0544a41
Alfredo Oliveira <alfredo.coj@gmail.com>
2019-04-02 12:12:27 -0300

    initial commit

---
+++ src/main/java/br/com/ithappens/components/MessageConsumer.java
@@ -0,0 +1,18 @@
+package br.com.ithappens.components;
+
+import br.com.ithappens.config.SpringAmqpConfig;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MessageConsumer {
+
+    private static final Logger logger = LoggerFactory.getLogger(MessageConsumer.class);
+
+    @RabbitListener(queues = {SpringAmqpConfig.queueName})
+    public void receiveMessage(String message) {
+        logger.info("Received Message: " + message);
+    }
+}
---
+++ src/main/java/br/com/ithappens/controllers/MessageController.java
@@ -0,0 +1,27 @@
+package br.com.ithappens.controllers;
+
+import br.com.ithappens.components.MessageProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Controller
+public class MessageController {
+
+    private final MessageProducer messageProducer;
+
+    @Autowired
+    public MessageController(MessageProducer messageProducer) {
+        this.messageProducer = messageProducer;
+    }
+
+    @RequestMapping(value="/messages", method= RequestMethod.POST)
+    @ResponseStatus(value= HttpStatus.CREATED)
+    public void sendMessage(@RequestBody String message) {
+        messageProducer.sendMessage(message);
+    }
+}
